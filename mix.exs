07410b6f4ddf186d48cb081f38dc9b92e2592742
conflagrate <jm@memorici.de>
2022-01-20 20:10:47 +0000

    Problem: divergence of deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uptight.MixProject do
   def project do
     [
       app: :uptight,
-      version: "0.1.0-pre",
+      version: "0.1.0-pre1",
       description:
         "Tools for tighter (more static) programming in Elixir with a particular focus on distinguishing types of binary data and pushing offensive programming capabilities to their limits.",
       elixir: "~> 1.12",
@@ -27,7 +27,7 @@ defmodule Uptight.MixProject do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.1.0", [runtime: false]},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
       {:doma_witchcraft, "~> 1.0.4-doma"},
       {:doma_algae, "~> 1.3.1-doma"},
       {:doma_quark, "~> 2.3.2-doma2"}
